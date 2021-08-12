d052bf066433b8496fdad65f689a12cd029f3903
chencongye <756311868@qq.com>
2021-08-12 18:56:00 +0800

    增加我的界面 v1

---
+++ app/src/main/java/com/znlccy/erudite/activity/SettingActivity.java
@@ -0,0 +1,22 @@
+package com.znlccy.erudite.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.znlccy.erudite.R;
+
+/**
+  * Author: znlccy
+  * Date: 2019-10-10 23:39
+  * Version: 1.0.0
+  * Comment: 设置界面
+  */
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
